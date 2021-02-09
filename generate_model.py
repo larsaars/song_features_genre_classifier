2683d9ad5d0a72c62c1e686c90dea6ce5bf0669b
Lurzapps <lurzapps@gmail.com>
2021-02-09 19:35:49 +0100

    train model

--- generate_model.py
+++ generate_model.py
@@ -1,8 +1,7 @@
-import numpy as np
 import pandas as pd
-from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
 import pickle as pkl
 
 # read df, make genres to str lists and drop all empty genres lists then
@@ -21,7 +20,7 @@ trainY, testY = train.genre, test.genre
 trainX, testX = train.drop(columns=['genre']), test.drop(columns=['genre'])
 
 # create the model
-model = RandomForestClassifier(n_estimators=100, verbose=2, n_jobs=3)
+model = KNeighborsClassifier(n_jobs=1)  # verbose = 2
 # # create training time estimator
 # estimator = Estimator(meta_algo='RF', verbose=3)
 # # run the estimation
@@ -32,7 +31,7 @@ model = RandomForestClassifier(n_estimators=100, verbose=2, n_jobs=3)
 model.fit(X=trainX, y=trainY)
 
 # dump model into file after generating
-with open('models/model.pkl') as file:
+with open('models/model.pkl', 'wb') as file:
     pkl.dump(model, file)
 
 # print test score
--- spotify_get.py
+++ spotify_get.py
@@ -1,18 +1,48 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+import pickle as pkl
+from sklearn.neighbors import KNeighborsClassifier
 
-search = input('Get data for search:')
-
+# load credentials
 with open('spotify_secrets.txt', 'r') as file:
     content = file.read().splitlines()
     client_id = content[0]
     client_secret = content[1]
 
+# login
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=client_id,
                                                            client_secret=client_secret))
 
-results = sp.search(q=search, limit=5)
-tracks = []
-for idx, track in enumerate(results['tracks']['items']):
-    ft = sp.audio_features([track['id']])
-    print(ft)
+# load model
+print('<< loading model...')
+model: KNeighborsClassifier
+with open('models/model.pkl', 'rb') as file:
+    model = pkl.load(file)
+
+# print when done loading
+print('<< Done! Enter a song name:')
+
+# loop prediction for input
+while True:
+    inp = input('>> ')
+
+    # break if input is 'q'
+    if inp == 'q':
+        break
+
+    # get first result
+    song_results = sp.search(q=inp, limit=1)
+
+    # call in loop for auto skipping if no results
+    for idx, track in enumerate(song_results['tracks']['items']):
+        # print the name and artist of track
+        artists = []
+        for idx2, artist in enumerate(track['artists']):
+            artists.append(artist['name'])
+
+        print('<< %s - %s' % (track['name'], artists[0] if len(artists) == 1 else str(artists)))
+        # else predict from features
+        # get the features
+        ft = sp.audio_features(track['id'])
+        # extract to list to be predictable
+        print(ft)
